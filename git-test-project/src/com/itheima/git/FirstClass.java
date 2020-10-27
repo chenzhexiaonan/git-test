b0d1c26fae5da7a830e73d70933ea93944b63133
chenzhenan <710782928@qq.com>
2020-10-27 10:01:08 +0800

    把本地工程添加到git

---
+++ git-test-project/src/com/itheima/git/FirstClass.java
@@ -0,0 +1,16 @@
+package com.itheima.git;
+
+/**
+ * @author: czn
+ * @create: 2020/10/27
+ * @Description:
+ * @FileName: FirstClass
+ * @History:
+ * @自定义内容：
+ */
+public class FirstClass {
+
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+    }
+}
